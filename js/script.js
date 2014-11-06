e33804216b01b742196f3055eaea1e961cf3c3d2
emileeapple <emilee.serafine@gmail.com>
2014-11-06 13:48:44 -0800

    Added second cloud

--- js/script.js
+++ js/script.js
@@ -24,6 +24,10 @@ function setup(){
 	cloud = paper.project.importSVG(document.getElementById('cloud'));
 	cloud.position.y = random(0, 200);
 	cloud.accum = random( .25, 2);
+
+	cloud2 = paper.project.importSVG(document.getElementById('cloud2'));
+	cloud2.position.y = random(100, 400);
+	cloud2.accum = random( 2, 4);
     
     radsq = pow(windowWidth/2,2)+pow(windowHeight,2);
     dia = sqrt(radsq);
@@ -60,10 +64,17 @@ function draw(){
 		winter.opacity = fgrad;
 	};
 
-	cloud.position.x += 10*MP + cloud.accum;
+	cloud.position.x += 6*MP + cloud.accum;
 	if(cloud.position.x >= windowWidth){
 		cloud.position.x=0
 		cloud.position.y = random(0, 200);
+
+	}
+
+	cloud2.position.x += 2*MP + cloud2.accum;
+	if(cloud2.position.x >= windowWidth){
+		cloud2.position.x=0
+		cloud2.position.y = random(100, 400);
 
 	}
 
