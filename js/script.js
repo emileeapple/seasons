a58a92b62444cd7ebf317253a962f177f7d8e094
SamHyde <samhydeevans@gmail.com>
2014-11-05 22:59:23 -0800

    Stuff

---
+++ js/script.js
@@ -0,0 +1,62 @@
+
+var grad;
+var gradtwo;
+var radsq;
+var dia;
+var winter;
+var spring;
+var MP;
+var wo;
+var fgrad;
+
+function setup(){
+	createCanvas(windowWidth, windowHeight);
+	var canvas = document.getElementById('defaultCanvas');
+	paper.setup(canvas);
+	grad = paper.project.importSVG(document.getElementById('svg'));
+    
+    radsq = pow(windowWidth/2,2)+pow(windowHeight,2);
+    dia = sqrt(radsq);
+
+    grad.scale(dia*2);
+
+    grad.position = new paper.Point(windowWidth/2,windowHeight);
+
+    //spring
+    spring = paper.project.importSVG(document.getElementById('spring'));
+
+    spring.position = new paper.Point(windowWidth/2,windowHeight/1.3);
+
+    //winter
+    winter = paper.project.importSVG(document.getElementById('winter'));
+
+    winter.position = new paper.Point(windowWidth/2,windowHeight/1.3);
+};
+
+function draw(){
+
+ MP = map(mouseX, 0,width,0,1);
+
+	grad.rotation += MP;
+
+
+	fgrad = map(MP,-1,1,0,1);
+
+	if(winter.opacity <= .01){
+		winter.opacity = fgrad * -1;
+	};
+	if(winter.opacity <= 1){
+		winter.opacity = fgrad;
+	};
+
+
+
+
+
+
+	paper.view.draw();
+
+
+
+
+};
